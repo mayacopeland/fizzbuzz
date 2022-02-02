47a746bfdfcb6d906c35a030df3c17fbbb4a8678
Maya Copeland <maya.copeland@protonmail.com>
2022-02-02 23:19:20 +0000

    is this more 'functional'

--- main.hs
+++ main.hs
@@ -1,28 +1,25 @@
 import System.Environment
-fb :: [Int] -> IO()
-fb (x:[]) = do
-        if x `mod` 15 == 0 then
-                putStrLn "FizzBuzz"
+
+createFizz :: Int -> String
+createFizz x =  
+		if x `mod` 15 == 0 then
+                "FizzBuzz"
         else if x `mod` 3 == 0 then
-                putStrLn "Fizz"
+				"Fizz"
         else if x `mod` 5 == 0 then
-                putStrLn "Buzz"
+                "Buzz"
         else
-		print x
-fb (x:xs) = do
-	if x `mod` 15 == 0 then 
-		putStrLn "FizzBuzz"
-	else if x `mod` 3 == 0 then
-		putStrLn "Fizz"
-	else if x `mod` 5 == 0 then
-		putStrLn "Buzz"
-	else
-		print x
-	fb xs
+			show x
 
+outputFizz :: [String] -> IO()
+outputFizz (x:[]) = putStrLn x 
+outputFizz (x:xs) = do 
+	putStrLn x 
+	outputFizz xs
 
-main :: IO ()
+main :: IO()
 main = do
 	args <- getArgs
 	let numbers = [1..(read $ head args)]
-	fb numbers
+	let xs = map createFizz numbers
+	outputFizz xs
