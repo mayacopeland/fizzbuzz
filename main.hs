2d29b8c291c552edb3c0b84a5ec7cbf8d756fc2d
Maya Copeland <maya.copeland@protonmail.com>
2022-02-02 23:23:34 +0000

    use spaces instead of tabs

--- main.hs
+++ main.hs
@@ -2,24 +2,24 @@ import System.Environment
 
 createFizz :: Int -> String
 createFizz x =  
-		if x `mod` 15 == 0 then
-                "FizzBuzz"
+        if x `mod` 15 == 0 then
+            "FizzBuzz"
         else if x `mod` 3 == 0 then
-				"Fizz"
+            "Fizz"
         else if x `mod` 5 == 0 then
-                "Buzz"
+            "Buzz"
         else
-			show x
+            show x
 
 outputFizz :: [String] -> IO()
 outputFizz (x:[]) = putStrLn x 
 outputFizz (x:xs) = do 
-	putStrLn x 
-	outputFizz xs
+    putStrLn x 
+    outputFizz xs
 
 main :: IO()
 main = do
-	args <- getArgs
-	let numbers = [1..(read $ head args)]
-	let xs = map createFizz numbers
-	outputFizz xs
+    args <- getArgs
+    let numbers = [1..(read $ head args)]
+    let xs = map createFizz numbers
+    outputFizz xs
